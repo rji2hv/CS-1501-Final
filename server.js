89a47e198f98584bd97fc2f94dc8ae69e1364402
vagrant <vagrant@precise32.(none)>
2014-02-09 18:08:29 +0000

    Commitment

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+var port = 8000;
+app.listen(port);
+console.log("Express app listening on port " + port);
+app.get("/", function(req, res){
+res.send('Hello, Welcome to CS1501!')
+});
